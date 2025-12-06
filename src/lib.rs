326cfe5ac0c3866c28d6d1a4a36f8ea160737b64
Muhammad Ragib Hasin <ragib.hasin@gmail.com>
2025-12-06 10:00:41 +0600

    update dependencies and edition

--- src/lib.rs
+++ src/lib.rs
@@ -29,11 +29,10 @@ pub fn generate_link(path: impl AsRef<std::path::Path>) -> Result<String, Error>
         .codeview_pdb70_debug_info
         .ok_or(Error::NoPdbInfo)?;
 
-    let filename = std::str::from_utf8(filename.split(|&c| c == b'\\').last().unwrap())
+    let filename = std::str::from_utf8(filename.split(|&c| c == b'\\').next_back().unwrap())
         .unwrap()
         .trim_end_matches('\0');
     let signature = format_uuid(signature);
-    let age = age;
 
     Ok(format!(
         "http://msdl.microsoft.com/download/symbols/{filename}/{signature}{age:X}/{filename}"
@@ -49,20 +48,4 @@ fn format_uuid(uuid: [u8; 16]) -> String {
         .chain(&uuid[8..])
         .map(|b| format!("{b:0>2X}"))
         .collect()
-}
-
-#[test]
-fn test_explorerextensions_dll_2A60F7AEC0AC46CFB343EA8931F562351() {
-    assert_eq!(
-        generate_link("ExplorerExtensions.2A60F7AEC0AC46CFB343EA8931F562351.dll").unwrap(),
-        "http://msdl.microsoft.com/download/symbols/ExplorerExtensions.pdb/2A60F7AEC0AC46CFB343EA8931F562351/ExplorerExtensions.pdb"
-    );
-}
-
-#[test]
-fn test_explorerextensions_dll_198CB2ADF93F4FE8BC0B6FB1A7D920D41() {
-    assert_eq!(
-        generate_link("ExplorerExtensions.198CB2ADF93F4FE8BC0B6FB1A7D920D41.dll").unwrap(),
-        "http://msdl.microsoft.com/download/symbols/ExplorerExtensions.pdb/198CB2ADF93F4FE8BC0B6FB1A7D920D41/ExplorerExtensions.pdb"
-    );
 }
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,6 @@
 fn main() {
-    let mut args = std::env::args_os();
-
     println!(
         "{}",
-        ms_pdb_linkgen::generate_link(&args.nth(1).unwrap()).unwrap()
+        ms_pdb_linkgen::generate_link(std::env::args_os().nth(1).unwrap()).unwrap()
     );
 }
